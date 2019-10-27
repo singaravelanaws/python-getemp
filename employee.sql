b6facceff8a8233100e8c3ccc64d416a2bdb820e
singaravelan palnai <singaravelan.redhat@gmail.com>
2019-10-27 16:09:57 +0530

    dbscripts

---
+++ employee.sql
@@ -0,0 +1,20 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 27, 2016 at 08:26 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+
+--
+-- Table structure for table `enotice_admin`
+--
+
+CREATE TABLE Employee (emp_id integer, first_name varchar(40), last_name varchar(40), primary_skills varchar(40), location char(10));
+
+--
+-- Dumping data for table `enotice_admin`
+--
+
